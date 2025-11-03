531494de207f85914e8c2a95295df50b132cc0bf
ivan-k2008 <korankyeivan@gmail.com>
2025-11-03 16:49:56 -0700

    Add files via upload

---
+++ Rock Paper Scissors Anew.py
@@ -0,0 +1,57 @@
+import random
+player_dic = {
+    }
+PlayAgain="yes"
+while PlayAgain==("yes"):
+
+
+        pscore=bscore=rounds=0
+        current_player = input("Who is the current player?")
+        if current_player not in player_dic:
+            CPS = 0
+            CBS = 0
+            player_dic[current_player] = (str(CPS) + ":" + str(CBS))
+        else:
+            CPS = player_dic[current_player].split(":")[0]
+            BPS = player_dic[current_player].split(":")[1]
+
+        
+        rounds=input("How many rounds would you like?")
+        while int(pscore) < int(rounds) and int(bscore) < int(rounds):
+            Bot=random.randint(1, 3)
+            Player = None
+            while Player == None:
+                Player=input("Rock, Paper, Scissors:")
+                if Player=='rock':
+                    Player=1
+                elif Player=='paper':
+                    Player=2
+                elif Player=='scissors':
+                    Player=3
+                else:
+                    print("please input a valid function")
+                    Player = None
+            Score=int(Player)-int(Bot)   
+            if Score==-1 or Score==2:
+                print("Loss!")
+                bscore += 1
+            elif Score==0:
+                print("Tie!")
+            else:
+                print("Win!")
+                pscore += 1
+
+                
+        if int(pscore) == int(rounds) and int(bscore) == int(rounds):
+            print("Tie??")
+        elif int(bscore) == int(rounds):
+            print("you have lost the game")
+        else:
+            print("you have won the game!!")
+        CPS = int(CPS) + pscore
+        CBS = int(CBS) + bscore
+        player_dic[current_player] = (str(CPS) + ":" + str(CBS))
+        print("\n")
+        PlayAgain=input("would you like to play again?")
+        
+
